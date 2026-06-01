9c79f3a9d2bc1661fa6ca90b31c7e0eec63e0cf5
Eniko Toth <totheniko060@gmail.com>
2026-06-01 20:21:41 +0300

    Email verification

--- src/lib/emailer.js
+++ src/lib/emailer.js
@@ -3,6 +3,7 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: parseInt(process.env.SMTP_PORT),
+    secure: true,
     auth: {
         user: process.env.SMTP_USER,
         password: process.env.SMTP_PASS,
